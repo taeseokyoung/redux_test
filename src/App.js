55a185b6e345587f53ccaa040ace926f4a12163d
taeseokyoung <xotjrud@gmail.com>
2022-10-14 11:55:56 +0900

    20221014

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import store from './store';
+
+const App = () => {
+    // console.log(store) 콘솔에 암것두 없어여
+    return (
+
+        <div> App
+
+        </div>
+    )
+}
+
+export default App;
---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+// configureStore
+const store = configureStore();
+
+export default store;
+
+// = configureStore({
+//     reducer: {},
+// })
