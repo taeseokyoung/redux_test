9ca4755822e160d470ab909b1e1c665e46a709fb
taeseokyoung <xotjrud@gmail.com>
2022-10-14 11:56:23 +0900

    20221014

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import store from './store';
 
 const App = () => {
     // console.log(store) 콘솔에 암것두 없어여
+    console.log(store)
     return (
 
-        <div> App
-
-        </div>
+        <div> App</div>
     )
 }
 
